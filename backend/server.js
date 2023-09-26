f659ae246d37eb93b5715bbffe0fa42e43113471
Oshan <it20070458@my.sliit.lk>
2023-09-26 15:34:55 +0530

    server implementation with mongo connection

---
+++ backend/server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+
+//Assign into localhost ports
+const PORT = process.env.PORT || 8000;
+
+//Json Methods
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.json());
+
+//Database Connection
+const URL = process.env.MONGODB_URL;
+
+//routes
+// app.use("/stores", require("./routes/inventory.route"));
+
+//MongoDB configurations
+
+mongoose.connect(URL, {
+    // useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+//   useFindAndModify: false,
+  });
+
+//Mongoose Connection
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB Connection Successful!");
+});
+
+//Run on port
+app.listen(PORT, () => {
+    console.log(`Server is up and running on port number : ${PORT}`);
+  });
