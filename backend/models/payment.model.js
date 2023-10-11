b3b2583618d43b22a4c2061866e42374136b2d86
Oshan <it20070458@my.sliit.lk>
2023-10-11 22:11:34 +0530

    update payment

--- backend/models/payment.model.js
+++ backend/models/payment.model.js
@@ -1,12 +1,7 @@
 const mongoose = require("mongoose");
-const { v4: uuidv4 } = require("uuid");
 
 const paymentSchema = new mongoose.Schema({
-    paymentID: {
-        type: String,
-        default: uuidv4, // Use uuidv4 to generate a unique ID
-        unique: true,   // Ensure uniqueness of the generated ID
-    },
+
     userID: {
         type: String,
         required: true
--- backend/models/prompt.model.js
+++ backend/models/prompt.model.js
@@ -1,12 +1,8 @@
 const mongoose = require("mongoose");
-const { v4: uuidv4 } = require("uuid");
+
 
 const promptSchema = new mongoose.Schema({
-    promptID: {
-        type: String,
-        default: uuidv4, // Use uuidv4 to generate a unique ID
-        unique: true,   // Ensure uniqueness of the generated ID
-    },
+
     userID: {
         type: String,
         required: true
